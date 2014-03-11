23c1fb9526eb1b4c608263a0d047653135046696
info-appmart <facebook@app-mart.jp>
2014-03-11 10:39:16 +0900

    hide info for swarm

--- src/com/canujohann/utils/Consts.java
+++ src/com/canujohann/utils/Consts.java
@@ -2,7 +2,7 @@ package com.canujohann.utils;
 
 public class Consts {
 
-	public static final int LEADERBOARD_ID  	= 14032;
-	public static final int SWARM_APP_ID  		= 6029;
+	public static final int LEADERBOARD_ID  	= 0000;
+	public static final int SWARM_APP_ID  		= 0000;
 	public static final String SWARM_APP_KEY  	= "f610370bc526a803f03c5608e67a4dfc";
 }
